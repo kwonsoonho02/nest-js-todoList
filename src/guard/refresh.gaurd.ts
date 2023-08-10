457e5ff2a90762e807f436c8c95a80309841dbea
kwonsoonho02 <53741635+kwonsoonho02@users.noreply.github.com>
2023-08-10 15:44:56 +0900

    backup

--- src/guard/refresh.gaurd.ts
+++ src/guard/refresh.gaurd.ts
@@ -3,12 +3,14 @@ import { UnauthorizedException } from '@nestjs/common/exceptions';
 import { CanActivate, ExecutionContext } from '@nestjs/common/interfaces';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { AuthService } from 'src/services/auth.services';
 
 @Injectable()
 export class RefreshGuard implements CanActivate {
   constructor(
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
+    private readonly authService: AuthService,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -16,12 +18,13 @@ export class RefreshGuard implements CanActivate {
     const res = context.switchToHttp().getResponse();
     const accessToken = req.cookies.accessToken;
     const refreshToken = req.cookies.refreshToken;
-
+    console.log(refreshToken);
     try {
+      console.log('토큰 확인 : ', accessToken);
       const accessTokenCheck = await this.jwtService.verifyAsync(accessToken, {
         secret: this.configService.get('accessToken'),
       });
-
+      console.log('토큰 체크 : ', accessTokenCheck);
       if (!accessTokenCheck) {
         const refreshTokenPayload = await this.jwtService.verifyAsync(
           refreshToken,
@@ -29,16 +32,19 @@ export class RefreshGuard implements CanActivate {
             secret: this.configService.get('refreshToken'),
           },
         );
-
-        if (!refreshTokenPayload) throw new UnauthorizedException();
+        const refreshId = refreshTokenPayload.id;
+        console.log(refreshId);
+        const refreshTokenCheck: boolean =
+          await this.authService.getUserIfRefreshTokenMatches(
+            refreshToken,
+            refreshId,
+          );
 
         const newAccessToken = this.jwtService.sign({
-          sub: refreshTokenPayload.sub,
+          sub: refreshId,
           secret: this.configService.get('accessToken'),
         });
-
         res.cookie('accessToken', newAccessToken, { httpOnly: true });
-
         return true;
       }
 
