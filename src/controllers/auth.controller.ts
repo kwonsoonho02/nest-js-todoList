3fdc5c6ed30c031be67b8bd17e2d45215912366a
kwonsoonho02 <53741635+kwonsoonho02@users.noreply.github.com>
2023-08-17 18:00:16 +0900

    Refactor: refresh token modify

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,6 +6,7 @@ import {
   Body,
   UseGuards,
   UnauthorizedException,
+  Header,
 } from '@nestjs/common';
 import { AuthService } from 'src/services/auth.services';
 import { Request, Response } from 'express';
@@ -14,21 +15,25 @@ import { User } from 'src/entities/users.entities';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { RefreshGuard } from 'src/guard/refresh.guard';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { hash } from 'bcrypt';
+import { Console } from 'console';
 
 @ApiTags('auth')
 @Controller()
 export class AuthController {
+  configService: any;
+  jwtService: any;
   constructor(private readonly authService: AuthService) {}
 
   @ApiOperation({ summary: 'User signup' })
   @Post('/signup')
-  async signUp(@Body() userData: CreateUserDTO, @Res() res: Response) {
+  async signUp(@Body() userData: CreateUserDTO) {
     try {
       const signUp: User = await this.authService.signUp(userData);
 
-      res.status(200).json({ data: signUp, msg: 'create' });
+      return signUp;
     } catch (error) {
-      console.log(error);
+      throw error;
     }
   }
 
@@ -39,18 +44,21 @@ export class AuthController {
       const userSignIn: User = await this.authService.signIn(userData);
 
       const accessToken: string = await this.authService.generateAccessToken(
-        userSignIn,
+        userSignIn.userId,
       );
       const refreshToken = await this.authService.generateRefreshToken(
         userSignIn,
       );
-      const getRefreshTokenDB: User = await this.authService.initRefreshTokenDB(
-        userSignIn,
-      );
+
       const getRefreshTokenExp = await this.authService.initRefreshTokenDBExp(
         userSignIn,
       );
-      const refreshTokenDB = getRefreshTokenDB.currentRefreshToken;
+
+      const hashedRefreshToken = await hash(refreshToken, 10);
+      userSignIn.currentRefreshToken = hashedRefreshToken;
+
+      const initRefreshTokenDB = await userSignIn.save();
+
       const refreshTokenExp = getRefreshTokenExp.currentRefreshTokenExp;
 
       res.cookie('accessToken', accessToken, { httpOnly: true });
@@ -59,43 +67,52 @@ export class AuthController {
       const responsePayload = {
         accessToken,
         refreshToken,
-        refreshTokenDB: refreshTokenDB,
-        refreshTokenExp: refreshTokenExp,
-        msg: 'signin',
+        currentRefreshToken: initRefreshTokenDB.currentRefreshToken,
+        currentRefreshTokenExp: refreshTokenExp,
+        msg: 'sign in',
       };
 
-      res.status(200).json(responsePayload);
+      return res.status(200).json(responsePayload);
     } catch (error) {
-      console.log(error);
+      throw error;
     }
   }
 
-  @ApiOperation({ summary: 'User signout' })
+  @ApiOperation({ summary: 'User sign out' })
   @UseGuards(AuthGuard)
   @Post('/signout')
   async signOut(@Res() res: Response, @Req() req: Request) {
     try {
       const userId: number = req['user'].id;
-      const deleteToken: User = await this.authService.signOut(userId);
       const clearDBRefreshToken = await this.authService.clearDBRefreshToken(
         userId,
       );
+
       res.clearCookie('authorization');
       res.clearCookie('accessToken');
       res.clearCookie('refreshToken');
-      res.status(200).json({
-        data: deleteToken,
-        clear: clearDBRefreshToken,
-        message: 'logout',
-      });
-    } catch (error) {}
+
+      const responsePayload = {
+        clearDBRefreshToken,
+        msg: 'sign out',
+      };
+
+      return res.status(200).json(responsePayload);
+    } catch (error) {
+      throw error;
+    }
   }
 
   @ApiOperation({ summary: 'Refresh token' })
   @UseGuards(RefreshGuard)
   @Post('/refresh')
-  async refresh() {
+  async refresh(@Req() req: Request) {
     try {
+      const refreshId = req['refreshId'];
+      console.log(refreshId);
+      const accessToken = await this.authService.generateAccessToken(refreshId);
+
+      return accessToken;
     } catch (err) {
       throw new UnauthorizedException();
     }
--- src/guard/refresh.guard.ts
+++ src/guard/refresh.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common/decorators';
+import { Injectable, Next } from '@nestjs/common/decorators';
 import { UnauthorizedException } from '@nestjs/common/exceptions';
 import { CanActivate, ExecutionContext } from '@nestjs/common/interfaces';
 import { ConfigService } from '@nestjs/config';
@@ -15,7 +15,6 @@ export class RefreshGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
-    const res = context.switchToHttp().getResponse();
     const refreshToken = req.cookies.refreshToken;
 
     try {
@@ -26,25 +25,11 @@ export class RefreshGuard implements CanActivate {
         },
       );
       const refreshId = refreshTokenPayload.id;
-      console.log('리프래쉬 토큰 아이디 확인 : ', refreshId);
       await this.authService.getUserIfRefreshTokenMatches(
         refreshToken,
         refreshId,
       );
-      console.log('리프래쉬 토큰 확인 완료');
-      const newAccessToken = this.jwtService.sign(
-        {
-          id: refreshId,
-        },
-        {
-          secret: this.configService.get<string>('accessToken'),
-        },
-      );
-      res.cookie('accessToken', newAccessToken, { httpOnly: true });
-      res.status(200).json({
-        accessToken: newAccessToken,
-        msg: '엑세스 토큰 재 발급 완료',
-      });
+      req['refreshId'] = refreshId;
     } catch (error) {
       console.error('RefreshGuard Error:', error.message);
       throw new UnauthorizedException('Unauthorized');
